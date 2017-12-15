95f82b7f1dbb0abae278c7a0c2801db66b7463a7
tasi788 <tasi788@gmail.com>
2017-12-15 16:23:58 +0800

    upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import request
+from flask import Flask
+import requests
+import json
+
+app = Flask(__name__)
+lookup = 'http://ip-api.com/json/{ip}?fields=520191&lang=en'
+@app.route('/')
+def hello_world():
+    agent = request.headers.get('User-Agent')
+    ip = request.remote_addr
+    try:
+        r = requests.get(lookup.format(ip=ip))
+        get = json.loads(r.text)
+        country = get['country']+get['city']
+        return '你用的裝置是{agent}\n你的IP是{ip}\n來自於{country}'.format(agent=agent,ip=ip,country=country)
+    except:
+        return '你用的裝置是{agent}\n你的IP是{ip}'.format(agent=agent,ip=ip)
+if __name__ == '__main__':
+    app.run(debug=True)
